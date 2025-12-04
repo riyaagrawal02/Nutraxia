d2f360e0a90f3075d4c40a8f141c4e323849d04e
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2025-12-05 01:05:07 +0530

    add MongoDB connection utility

---
+++ app/lib/mongodb.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+export const connectDB= async() => {
+    try{
+        if(mongoose.connection.readyState >= 1) return;
+        await mongoose.connect(process.env.MONGODB_URI as string);
+        console.log("MongoDB Connected");
+    }
+    catch(error){
+        console.log("MongoDB Error:", error);
+    }
+};
