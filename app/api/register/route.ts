cdf55984fc72b5cf42fc2f65f8dc08ee1d482de8
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2025-12-05 01:25:15 +0530

    add registration API

---
+++ app/api/register/route.ts
@@ -0,0 +1,28 @@
+import {NextResponse} from "next/server";
+import {connectDB} from "@/app/lib/mongodb";
+import User from "@/app/models/User";
+import bcrypt from "bcryptjs";
+export async function POST(req:Request){
+    try{
+        const{name,email, password}= await req.json();
+        await connectDB();
+
+        const existing = await User.findOne({email});
+        if(existing)
+            return NextResponse.json({
+        success:false, message:"User already exists"},
+    {status:400});
+    const hashed= await bcrypt.hash(password,10);
+    const user= await User.create({
+        name, email, password:hashed,
+    });
+    return NextResponse.json({success:true, user});
+    }
+    catch(error){
+        return NextResponse.json({
+            success:false, message:"Server error"
+        },{
+            status:500
+        });
+    }
+}
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -13,7 +13,18 @@ export default function Register(){
     const handleChange= (e:ChangeEvent<HTMLInputElement>)=>{setData({...data,[e.target.name]:e.target.value});
 };
     const handleRegister = async () => {
-    console.log(data);
+        try{
+            const res= await fetch("/api/register",{
+                method: 'POST',
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(data),
+            });
+            const result= await res.json();
+            console.log(result);
+        }
+        catch(error){
+            console.log(error);
+        }
   };
 
   return (
