87fd102dfe6e5612335c125c20cff74876968189
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-03-23 23:26:54 +0530

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppThemeProvider from "./providers/ThemeProviders";
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
             {children}
           </div>
         </AppThemeProvider>
+        <Analytics/>
       </body>
     </html>
   );
